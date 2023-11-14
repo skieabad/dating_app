970fa2c13c58ecf59333a8d538520631ca432dbb
Wilson Abad <wilsonabad@pangga.ph>
2023-11-15 01:48:53 +0800

    set up colors

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFE94057);
+  static const Color backgroundColor = Color(0xFFF3F3F3);
+  static const Color borderColor = Color(0xFFE8E6EA);
+}
