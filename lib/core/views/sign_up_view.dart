d6d17114970ba3a1944959cfb30b7714f6b44a96
Wilson Abad <wilsonabad@pangga.ph>
2023-11-15 01:49:43 +0800

    create sign up view

---
+++ lib/core/views/sign_up_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SignUpView extends StatelessWidget {
+  const SignUpView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text('Display here'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
