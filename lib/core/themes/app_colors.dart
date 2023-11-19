4b7875e1e546ec19fb91285993579796ff361fd0
Wilson Abad <wilsonabad@pangga.ph>
2023-11-20 01:22:54 +0800

    add secondary color and rename the image path

--- lib/core/themes/app_colors.dart
+++ lib/core/themes/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primaryColor = Color(0xFFE94057);
+  static const Color secondaryColor = Color(0xFF323755);
   static const Color backgroundColor = Color(0xFFF3F3F3);
   static const Color borderColor = Color(0xFFE1DEE4);
 }
